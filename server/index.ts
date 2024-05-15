5efe515eed0003b909edc61c4d1bb52c12a3f13a
Gintu Ciprian <ciprian.florin19g@gmail.com>
2024-05-15 23:45:17 +0300

    chore: change port

--- server/index.ts
+++ server/index.ts
@@ -1,9 +1,13 @@
 import http from "http";
 import { Router } from "./router";
 import { signInMiddleware, signUpMiddleware } from "./routes/auth";
+import { getYoutubeThumbnail } from './routes/images';
+
+//console.log(await getYoutubeThumbnail("REt5yDh8eGg"));
 
 const server = http.createServer((req, res) => {
   const router = new Router(req, res);
+
 
   router.post("/auth/signup", signUpMiddleware);
   router.post("/auth/signin", signInMiddleware);
