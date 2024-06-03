4885e9ce21f08045ec8cf282cb8aecf525eb014f
Gintu Ciprian <ciprian.florin19g@gmail.com>
2024-06-03 22:00:49 +0300

    feat: display imgs from db

--- server/index.ts
+++ server/index.ts
@@ -10,7 +10,8 @@ const server = http.createServer((req, res) => {
 
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  // headers - authorization
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type,Authorization');
   res.setHeader('Access-Control-Allow-Credentials', 'true');
 
   if (req.method === 'OPTIONS') {
